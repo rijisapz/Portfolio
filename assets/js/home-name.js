85e61c16d3848f5fde7355edb56a08158febb29d
Regie Sapico <regiesapico1607@gmail.com>
2023-05-13 10:25:26 -0700

    create home page

--- assets/js/home-name.js
+++ assets/js/home-name.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    var text = "Contact Me ";
+    var text = "regie sapico.";
     var i = 0;
     function typeWriter() {
       if (i < text.length) {
-        $('#me-contact').append(text.charAt(i));
+        $('#me-name').append(text.charAt(i));
         i++;
-        setTimeout(typeWriter, 400); // Change the delay time here
+        setTimeout(typeWriter, 300); // Change the delay time here
       }
       else {
         i = 0; // Reset i to 0 to start the loop again
-        $('#me-contact').empty(); // Clear the text for the next loop
-        $('#me-contact').append('<span>&nbsp;</span>');
+        $('#me-name').empty(); // Clear the text for the next loop
+        $('#me-name').append('<span>&nbsp;</span>');
         setTimeout(typeWriter, 0); // Add a longer delay between loops
       }
     }

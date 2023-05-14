fa08e4270402894337cd87a4ccc9e7f715538cb5
Regie Sapico <regiesapico1607@gmail.com>
2023-05-14 08:17:16 -0700

    fix typing issue

--- assets/js/home-name.js
+++ assets/js/home-name.js
@@ -1,18 +1,22 @@
 $(document).ready(function() {
-    var text = "regie sapico.";
-    var i = 0;
-    function typeWriter() {
-      if (i < text.length) {
-        $('#me-name').append(text.charAt(i));
-        i++;
-        setTimeout(typeWriter, 300); // Change the delay time here
-      }
-      else {
-        i = 0; // Reset i to 0 to start the loop again
-        $('#me-name').empty(); // Clear the text for the next loop
-        $('#me-name').append('<span>&nbsp;</span>');
-        setTimeout(typeWriter, 0); // Add a longer delay between loops
-      }
+  var text = "regie sapico.";
+  var i = 0;
+  function typeWriter() {
+    if (i < text.length) {
+      $('#me-name').append(text.charAt(i));
+      i++;
+      setTimeout(typeWriter, 300); // Change the delay time here
+    } else {
+      i = 0; // Reset i to 0 to start the loop again
+      $('#me-name').empty(); // Clear the text for the next loop
+      $('#me-name').append('<span>&nbsp;</span>');
+      setTimeout(typeWriter, 0); // Add a longer delay between loops
     }
-    typeWriter();
-  });
+  }
+  $('#me-name').empty(); // Remove any existing text from the element
+
+  setTimeout(function() {
+    $('#me-name').show(); // Show the element before starting the animation
+    typeWriter(); // Start the animation
+  }, 01); // Add a 0.1ms delay before starting the animation
+});
